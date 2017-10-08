8eae80e0449365c1542819533aea6e87ea0ab34a
kzhrk <info@kzhrk.com>
2017-10-09 02:26:31 +0900

    initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,37 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+	entry: {
+		main: './src/webpack/js/main.js'
+	},
+
+	output: {
+		path: path.resolve(__dirname, './public/js'),
+		filename: '[name].js'
+	},
+
+	resolve: {
+		extensions: ['.js']
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				use: [{
+					loader: "babel-loader"
+				}]
+			}
+		]
+	},
+
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin()
+	]
+}
